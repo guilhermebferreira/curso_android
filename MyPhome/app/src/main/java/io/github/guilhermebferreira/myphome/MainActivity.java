22d45e6623e214108c6a93f87f10e164b57ac9c5
Guilherme Ferreira <360guilherme@gmail.com>
2018-07-18 16:10:05 -0300

    adicionado listener para acomapnhar as seleções ods items de menu

--- MyPhome/app/src/main/java/io/github/guilhermebferreira/myphome/MainActivity.java
+++ MyPhome/app/src/main/java/io/github/guilhermebferreira/myphome/MainActivity.java
@@ -1,13 +1,40 @@
 package io.github.guilhermebferreira.myphome;
 
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = findViewById(R.id.bottomNavigationView);
+        navigation.setOnNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item){
+
+        switch (item.getItemId()){
+            case R.id.profile:
+                Log.i("INFO", "click on PROFILE");
+                break;
+            case R.id.cardapio:
+                Log.i("INFO", "click on CARDAPIO");
+                break;
+            case R.id.about:
+                Log.i("INFO", "click on ABOUT");
+                break;
+        }
+
+        return true;
+
     }
 }
