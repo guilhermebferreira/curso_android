fcb1c1b674e11fa555afb1b49dc8eb76659cd65e
Guilherme Ferreira <360guilherme@gmail.com>
2018-07-19 15:39:52 -0300

    criando projeto para realizar persistencia em banco

---
+++ BdProject/app/src/main/java/io/github/guilhermebferreira/bdproject/ActivityForm.java
@@ -0,0 +1,13 @@
+package io.github.guilhermebferreira.bdproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form);
+    }
+}
